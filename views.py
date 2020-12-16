082874bb8f0de33b39b26adf32efaa1c873292ce
1855532 <aditi.s.akella@gmail.com>
2020-12-16 15:50:01 -0800

    addedd app routes for about us

--- views.py
+++ views.py
@@ -13,3 +13,16 @@ def feedback():
 def aboutus():
     #Flask import uses Jinga to render HTML
     return render_template("About.html", data=dataaboutus.alldata())
+
+@app.route('/aboutus/aditi/')
+def aditi():
+    #Flask import uses Jinga to render HTML
+    return render_template("aditi.html", data=dataaboutus.aditis_info())
+@app.route('/aboutus/sophie/')
+def sophie():
+    #Flask import uses Jinga to render HTML
+    return render_template("sophie.html", data=dataaboutus.sophies_info())
+@app.route('/aboutus/grace/')
+def grace():
+    #Flask import uses Jinga to render HTML
+    return render_template("grace.html", data=dataaboutus.graces_info())
