c2a03559fc18edcbd1b9ba131eb99e832c2dc9f6
1855532 <aditi.s.akella@gmail.com>
2020-12-16 15:39:52 -0800

    about us

---
+++ dataaboutus.py
@@ -0,0 +1,28 @@
+def aditis_info():
+    name = "Aditi Akella"
+    grade = "11th grade"
+    about = "Hello! I am Aditi and I am a Junior at Del Norte. I love animals, travel, and science. This is my first year in Mr. M's class and I am learning so much about Python and html files! I look forward to learning more about computers and programming."
+    contributions = "Fill in"
+    info = {"name": name, "grade": grade, "about": about, "contributions": contributions}
+    return info
+
+def sophies_info():
+    greeting= "Hi!"
+    name = "Sophie Bulkin"
+    grade = "11th grade"
+    about = "Hello! I am Sophie and I am an eleventh grader at Del Norte. I took a  Computer Science class last year with Mr.M and it has been a very fun experience. I have learned different types of coding from basic Python fundamentals to .html. I hope to further improve from here. My main contribution has been to the about us page and I have worked on fixing the dropdown button."
+    contributions = "Fill in"
+    info = {"greeting": greeting, "name": name, "grade": grade, "about": about, "contributions": contributions}
+    return info
+
+def graces_info():
+    greeting= "Hi!"
+    name = "Grace Le"
+    grade = "11th grade"
+    about = "Fill in"
+    contributions = "Fill in"
+    info = {"greeting": greeting, "name": name, "grade": grade, "about": about, "contributions": contributions}
+    return info
+
+def alldata():
+    return [aditis_info(), sophies_info(), graces_info]
--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import dataaboutus
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,5 +8,7 @@ def home():
 @app.route('/feedback/')
 def feedback():
     return render_template("Feedback.html")
-
-
+@app.route('/aboutus/')
+def aboutus():
+    #Flask import uses Jinga to render HTML
+    return render_template("aboutus.html", data=dataaboutus.alldata()
