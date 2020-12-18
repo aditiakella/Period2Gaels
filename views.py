5d4783036f580ea0d5b4fb9007ed1d94021653aa
1855532 <aditi.s.akella@gmail.com>
2020-12-18 09:28:51 -0800

    fixed phylogeny page

--- views.py
+++ views.py
@@ -26,6 +26,7 @@ def sophie():
 def grace():
     #Flask import uses Jinga to render HTML
     return render_template("grace.html", data=dataaboutus.graces_info())
+
 @app.route('/Phylogenetic/')
 def Phylogenetic():
-    return render_template(Phylogenetic.html)
+    return render_template("Phylogenetic.html")
