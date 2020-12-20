6c0bdc4bbcddcaef289a9ee36452b6779bf5610a
1855532 <aditi.s.akella@gmail.com>
2020-12-20 11:44:57 -0800

    changed about us info

--- dataaboutus.py
+++ dataaboutus.py
@@ -2,7 +2,7 @@ def aditis_info():
     name = "Aditi Akella"
     grade = "11th grade"
     about = "Hello! I am Aditi and I am a Junior at Del Norte. I love animals, travel, and science. This is my first year in Mr. M's class and I am learning so much about Python and html files! I look forward to learning more about computers and programming."
-    contributions = "Fill in"
+    contributions = "Contributions: I created the about us page and the Jinja templates, and I also added most of the app routes on views.py and passed data through the back end."
     info = {"name": name, "grade": grade, "about": about, "contributions": contributions}
     return info
 
@@ -11,7 +11,7 @@ def sophies_info():
     name = "Sophie Bulkin"
     grade = "11th grade"
     about = "Hello! I am Sophie and I am an eleventh grader at Del Norte. I took a  Computer Science class last year with Mr.M and it has been a very fun experience. I have learned different types of coding from basic Python fundamentals to .html. I hope to further improve from here."
-    contributions = "about us page, prototypes,sophie.html, feedback"
+    contributions = "Contributions: about us page, prototypes,sophie.html, feedback"
     info = {"greeting": greeting, "name": name, "grade": grade, "about": about, "contributions": contributions}
     return info
 
@@ -20,7 +20,7 @@ def graces_info():
     name = "Grace Le"
     grade = "11th grade"
     about = "Hi my name is Grace"
-    contributions = "Fill in"
+    contributions = "Contributions: Fill in"
     info = {"greeting": greeting, "name": name, "grade": grade, "about": about, "contributions": contributions}
     return info
 
